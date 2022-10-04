ba5bba4d090366c6dc269cb977176593b9ffc7ed
marieKarnaukh <114482368+marieKarnaukh@users.noreply.github.com>
2022-10-04 22:54:02 +0300

    1717B

---
+++ karnaukh_m_v/1717b.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+ 
+int main()
+{
+    int t = 0, n = 0, k = 0, r = 0, c = 0, p=1;
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n >> k >> c >> r;
+        //p = (1 + abs(c%k - r%k))%k;
+        for (int h = 1; h <= k; h++)
+        {
+            if ((h + c - 1) % k == r%k)
+                p = h%k;
+        }
+        for (int h = 1; h <= n; h++)
+        {
+            for (int g = 1; g <= n; g++)
+            {
+                //if (h != c)
+                //{
+                    if (g % k == p || k==1)
+                        std::cout << "X";
+                    else
+                    {
+                        std::cout << ".";
+                    }
+                //}
+                /*else
+                {
+                    if (g % k == r % k)
+                        std::cout << "X";
+                    else
+                    {
+                        std::cout << ".";
+                    }
+                    //p = r % k + 1;
+         
+ 
+ 
+                }*/
+ 
+            }
+            p++;
+            p %= k;
+            std::cout << std::endl;
+        }
+ 
+    }
+}
