512d126180468aee494f8ee68f188678417e5e5d
marieKarnaukh <114482368+marieKarnaukh@users.noreply.github.com>
2022-10-13 13:46:53 +0300

    935B

---
+++ karnaukh_m_v/935b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+ 
+int main()
+{
+    int n = 0;
+    char step;
+    std::cin >> n;
+    int x = 0;
+    int y = 0;
+    char flag = '0';
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> step;
+        if (step == 'U')
+        {
+            y += 1;
+        }
+        else x += 1;
+        
+        if (flag == step)
+        {
+            k += 1;
+        }
+        flag = '0';
+        if (x == y)
+        {
+            flag = step;
+        }
+        
+    }
+    std::cout << k << "\n";
+}
